3aab6d1c890a225c064d05375aaa88bfa8f650c3
ronewa <ronewamaselesele200@gmail.com>
2024-05-14 00:51:18 +0200

    This class will be used to store default value needed in this application before it goes live

--- app/src/main/java/com/example/collectibles_den/DefaultValuesClass.kt
+++ app/src/main/java/com/example/collectibles_den/DefaultValuesClass.kt
@@ -11,43 +11,43 @@ class DefaultValuesClass {
             collectionID = "laskdk",
             collectionName = "kajsnldpaks",
             collectionDescription = listOf("kdoaopdkso","sdkajsoas"),
-            posted_Date = Timestamp(System.currentTimeMillis())
+            postedDate = Timestamp(System.currentTimeMillis())
         ),
         collectionStorage(
             collectionID = "7uyb7",
             collectionName = "sduijosp",
             collectionDescription = listOf("skdifsj","sfafoif"),
-            posted_Date = Timestamp(System.currentTimeMillis())
+            postedDate = Timestamp(System.currentTimeMillis())
         ),
         collectionStorage(
             collectionID = "49djd",
             collectionName = "afoiajp-kdf",
             collectionDescription = listOf("adfkaoisfj","afkoaidjiaks"),
-            posted_Date = Timestamp(System.currentTimeMillis())
+            postedDate = Timestamp(System.currentTimeMillis())
         ),
         collectionStorage(
             collectionID = "al820391j",
             collectionName = "afoaijd",
             collectionDescription = listOf("svodiv","akfiuoaij"),
-            posted_Date = Timestamp(System.currentTimeMillis())
+            postedDate = Timestamp(System.currentTimeMillis())
         ),
         collectionStorage(
             collectionID = "89yhj hk",
             collectionName = "a56yughjjhfoaijd",
             collectionDescription = listOf("kyutr457689yo","dtry76875erdfghjkl;ihugyftyugi"),
-            posted_Date = getBeginningOfPastMonth()
+            postedDate = getBeginningOfPastMonth()
         ),
         collectionStorage(
             collectionID = "8ytyuij",
             collectionName = "htu86rtdgfhj",
             collectionDescription = listOf("=0ijhiyb","l;opiuyfgihoj;jhtr"),
-            posted_Date = getBeginningOfPastMonth()
+            postedDate = getBeginningOfPastMonth()
         ),
         collectionStorage(
             collectionID = "nbgyu876trdf",
             collectionName = "8765rdcv",
             collectionDescription = listOf("0987tcvbjkl;","retyu"),
-            posted_Date = getBeginningOfPastMonth()
+            postedDate = getBeginningOfPastMonth()
         ),
     )
 }
