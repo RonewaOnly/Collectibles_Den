2b9dc83d3866201a71a3d6de703eb58cb5f977b4
ronewa <ronewamaselesele200@gmail.com>
2024-05-14 04:35:57 +0200

    A class that will be used to take the values and functions needed by the storyboard function

---
+++ app/src/main/java/com/example/collectibles_den/Data/Storyboard_Stories.kt
@@ -0,0 +1,28 @@
+package com.example.collectibles_den.Data
+
+import android.net.Uri
+import java.sql.Timestamp
+
+class Storyboard_Stories {
+
+    data class StoryboardLine(
+        val storyID: String ="",
+        val storyName: String,
+        val storyItems: List<collectionStorage?> = emptyList(),
+        val storyDescription: String = "",
+        val storyCovers: List<Uri?> = emptyList(),
+        val goalSet: Int = 0,
+        val dateOfCreation: Timestamp = Timestamp(System.currentTimeMillis()),
+        val tags: List<String> = emptyList(),
+        val storyCategory: String = ""
+    )
+
+    data class Settings(//This data class will be
+        val storyId : StoryboardLine,
+        val onEdit: () -> Unit,
+        val onDelete: () -> Unit,
+        val onShare: () -> Unit,
+        val onSave: () -> Unit
+    )
+
+}
