13428e8549549724ccfa6aca2fb1b3c6448357c3
ronewa <ronewamaselesele200@gmail.com>
2024-05-15 01:33:27 +0200

    changed from read-only to write-read so (val to var)

--- app/src/main/java/com/example/collectibles_den/Data/UserData.kt
+++ app/src/main/java/com/example/collectibles_den/Data/UserData.kt
@@ -1,10 +1,10 @@
 package com.example.collectibles_den.Data
 
 data class UserData(
-    val customerId: String,
-    val firstname: String,
-    val lastname: String,
-    val username: String,
-    val email: String,
-    val password: String,
+    val customerId: String ="",
+    var firstname: String="",
+    var lastname: String="",
+    var username: String,
+    var email: String,
+    var password: String,
 )
