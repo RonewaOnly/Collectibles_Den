800be1abc8c4c687840719d84f17839fdc646ee8
ronewa <ronewamaselesele200@gmail.com>
2024-05-14 04:39:14 +0200

    removed a implementation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,7 +58,6 @@ dependencies {
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
     implementation("androidx.compose.material:material-icons-extended:1.6.7")
     //ML Kit document scanner
     implementation("com.google.android.gms:play-services-mlkit-document-scanner:16.0.0-beta1")
@@ -66,6 +65,7 @@ dependencies {
     implementation("io.coil-kt:coil-compose:2.5.0")
     implementation(libs.androidx.navigation.runtime.ktx)
     implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.material3.android)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
