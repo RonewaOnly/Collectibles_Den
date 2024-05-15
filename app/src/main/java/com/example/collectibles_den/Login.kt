9e4cdbad6ccaead682e2d623e320c035db60e316
RC_Student_lab <ST10104119@rcconnect.edu.za>
2024-05-15 15:26:20 +0200

    Onclick function to login file

--- app/src/main/java/com/example/collectibles_den/Login.kt
+++ app/src/main/java/com/example/collectibles_den/Login.kt
@@ -3,18 +3,61 @@ package com.example.collectibles_den
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
 
 class Login : AppCompatActivity() {
+
+    private lateinit var tabLayout: TabLayout
+    private lateinit var viewPager2: ViewPager2
+    private lateinit var adapter: Fragement
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+
+            tabLayout = findViewById(R.id.tab_layout)
+            viewPager2 = findViewById(R.id.viewPager)
+
+            adapter = Fragement(supportFragmentManager, lifecycle)
+
+            tabLayout.addTab(tabLayout.newTab().setText("Login"))
+            tabLayout.addTab(tabLayout.newTab().setText("Register"))
+
+            viewPager2.adapter = adapter
+
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+
+                if (tab != null) {
+                    viewPager2.currentItem = tab.position
+                }
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+
+            }
+
+        })
+
+        //
+        viewPager2.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback(){
+
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+
+                tabLayout.selectTab(tabLayout.getTabAt(position))
+            }
+
+        })
+
+
         }
-    }
+
+
 }
