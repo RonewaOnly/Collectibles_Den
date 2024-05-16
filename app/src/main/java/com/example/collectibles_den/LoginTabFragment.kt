eaabe25588e9b57b41e8cae9a569d0f7eac71103
RC_Student_lab <ST10104119@rcconnect.edu.za>
2024-05-16 15:51:19 +0200

    Onclick function with toast message

--- app/src/main/java/com/example/collectibles_den/LoginTabFragment.kt
+++ app/src/main/java/com/example/collectibles_den/LoginTabFragment.kt
@@ -4,17 +4,28 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 
 class LoginTabFragment : Fragment() {
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login_tab, container, false)
-    }
+        val view = inflater.inflate(R.layout.fragment_login_tab, container, false)
 
+        //onclick function
+        view.findViewById<Button>(R.id.login_button).setOnClickListener{
+
+            Toast.makeText(activity,"Logged in successful", Toast.LENGTH_SHORT).show();
+
+        }
+
+        return view
+    }
 
 }
