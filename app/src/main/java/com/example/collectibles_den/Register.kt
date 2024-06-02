bacdfe969db529a53c884efab95c47e9a5164612
ST10061533 <105481917+ST10061533@users.noreply.github.com>
2024-06-02 21:27:12 +0200

    Form validation

--- app/src/main/java/com/example/collectibles_den/Register.kt
+++ app/src/main/java/com/example/collectibles_den/Register.kt
@@ -10,6 +10,7 @@ import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.example.collectibles_den.databinding.ActivityRegisterBinding
 import com.google.android.material.textfield.TextInputEditText
+import android.util.Patterns as Patterns
 
 class Register : AppCompatActivity() {
 
@@ -29,6 +30,12 @@ class Register : AppCompatActivity() {
         //assign binding
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        //function to valid input fields
+        emailFocusListener()
+        passwordFocusListener()
+        namesFocusListener()
+        lastNameFocusListener()
 
         //declaring text field inputs
         val firstname = findViewById<TextInputEditText>(R.id.signup_firstName)
@@ -82,6 +89,19 @@ class Register : AppCompatActivity() {
 
 
             }else{
+
+                //clearing form after successful validation
+                binding.signupFirstName.text = null
+                binding.signupLastName.text = null
+                binding.signupEmail.text = null
+                binding.signupPassword.text = null
+                binding.signupConfirmPassword.text = null
+                binding.signupFirstNameContainer.helperText = null
+                binding.signupLastNameContainer.helperText = null
+                binding.signupEmailContainer.helperText = null
+                binding.signupPasswordContainer.helperText = null
+                binding.signupConfirmPasswordContainer.helperText = null
+
 
                 Toast.makeText(this, "hey $firstnameInput $lastnameInput", Toast.LENGTH_SHORT).show()
                 //registerViewModel.registration(firstnameInput,lastnameInput,emailInput,passwordInput,confirmPasswordInput)
@@ -140,5 +160,155 @@ class Register : AppCompatActivity() {
 //        }
 
 
+    }
+
+    //function to valid last name
+    private fun lastNameFocusListener() {
+
+        binding.signupLastName.setOnFocusChangeListener { _, focused ->
+
+            if(!focused){
+
+                binding.signupLastNameContainer.helperText = validLastName()
+            }
+        }
+    }
+
+    private fun validLastName(): String? {
+
+        //declaring variable for user names
+        val lastName = binding.signupLastName.text.toString()
+
+        //if statement to check names
+        if(lastName.length < 3){
+
+            return "Minimum 3 Characters"
+        }
+        if(lastName.matches(".*[0-9].*".toRegex())){
+
+            return "Can not contain a number"
+        }
+        if(lastName.matches(".*[@#\$%^&+=].*".toRegex())){
+
+            return "Can not contain special characters"
+        }
+
+        //return statement
+        return null
+    }
+
+    //function to valid first name
+    private fun namesFocusListener() {
+
+        binding.signupFirstName.setOnFocusChangeListener { _, focused ->
+
+            //if not focused
+            if(!focused){
+
+                binding.signupFirstNameContainer.helperText = validName()
+            }
+
+        }
+    }
+
+    private fun validName(): String? {
+
+        //declaring variable for user names
+        val firstName = binding.signupFirstName.text.toString()
+
+
+        //if statement to check names
+        if(firstName.length < 3){
+
+            return "Minimum 3 Characters"
+        }
+        if(firstName.matches(".*[0-9].*".toRegex())){
+
+            return "Can not contain a number"
+        }
+        if(firstName.matches(".*[@#\$%^&+=].*".toRegex())){
+
+            return "Can not contain special characters"
+        }
+
+        //return statement
+        return null
+    }
+
+    //function to valid password
+    private fun passwordFocusListener() {
+
+        binding.signupPassword.setOnFocusChangeListener { _, focused ->
+
+            //if not focused
+            if(!focused){
+
+                binding.signupPasswordContainer.helperText = validPassword()
+            }
+        }
+    }
+
+    //function to valid password
+    private fun validPassword(): String? {
+
+        //declare a variable for password input field
+        val password = binding.signupPassword.text.toString()
+
+        //if statement to check for password characters
+        if(password.length < 8){
+
+            return "Minimum 8 characters"
+
+        }
+        if(!password.matches(".*[A-Z].*".toRegex())){
+
+            return "Must Contain Uppercase letter"
+        }
+        if(!password.matches(".*[a-z].*".toRegex())){
+
+            return "Must Contain lowercase letter"
+        }
+        if(!password.matches(".*[@#\$%^&+=].*".toRegex())){
+
+            return "Must Contain special characters"
+        }
+        if(!password.matches(".*[0-9].*".toRegex())){
+
+            return "Must Contain at a number"
+        }
+
+        //return statement
+        return null
+    }
+
+
+    //function to listen email input
+    private fun emailFocusListener() {
+
+        binding.signupEmail.setOnFocusChangeListener { _, focused ->
+
+            //if not focused
+            if(!focused){
+
+                binding.signupEmailContainer.helperText = validEmail()
+            }
+        }
+    }
+
+    //function to valid email
+    private fun validEmail(): String? {
+
+        //declaring a variable
+        val email = binding.signupEmail.text.toString()
+
+        //if email address domain is invalid
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+
+            //returning a string message
+            return "Invalid Email address"
+        }
+
+        //return statement
+        return null
     }
 }
