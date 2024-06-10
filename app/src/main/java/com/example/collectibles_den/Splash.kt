465816b3031417aa01f4213b51ae7134f49150e1
ST10061533 <105481917+ST10061533@users.noreply.github.com>
2024-06-10 16:46:25 +0200

    splash screen update

---
+++ app/src/main/java/com/example/collectibles_den/Splash.kt
@@ -0,0 +1,16 @@
+package com.example.collectibles_den
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Splash : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash)
+
+    }
+}
