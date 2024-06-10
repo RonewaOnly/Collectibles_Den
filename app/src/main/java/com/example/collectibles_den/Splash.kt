f00c8b04b429f22a3a4473295950c70ba7a37539
ST10061533 <105481917+ST10061533@users.noreply.github.com>
2024-06-10 17:43:41 +0200

    Splash screen added

--- app/src/main/java/com/example/collectibles_den/Splash.kt
+++ app/src/main/java/com/example/collectibles_den/Splash.kt
@@ -1,6 +1,8 @@
 package com.example.collectibles_den
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
@@ -9,6 +11,18 @@ class Splash : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_splash)
+
+        //set a handler
+        Handler().postDelayed({
+
+            //intent to navigate to start activity
+            val intent = Intent(this@Splash, StartActivity::class.java)
+            startActivity(intent)
+
+            //finish the method
+            finish()
+
+        }, 3000)
 
     }
 }
