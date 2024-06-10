2dcf84c9e74919db8b1dccb4d7582626a6f5054c
ST10061533 <105481917+ST10061533@users.noreply.github.com>
2024-06-10 16:48:13 +0200

    removed un used imports

--- app/src/main/java/com/example/collectibles_den/Splash.kt
+++ app/src/main/java/com/example/collectibles_den/Splash.kt
@@ -3,8 +3,6 @@ package com.example.collectibles_den
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class Splash : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
